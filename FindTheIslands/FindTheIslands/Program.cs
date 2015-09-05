8a37ea5e29b67e4c8a58e2e5e4f2f8fbbdec4e38
Cody <codywahl@gmail.com>
2015-09-04 20:48:41 -0400

    Commiting the solution I came up with the interview. 1 known issue.
    0 0 1
    0 1 1
    0 0 1
    
    In this situation, the application would count 2 islands. I'm currently
    only checking the above and left for islands, when in this edge case it
    would need to have checked to the right as well to be accurate. The
    right would have only been added to the visited list if we checked
    below. In short, I need to check in all directions.

---
+++ FindTheIslands/FindTheIslands/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace FindTheIslands
+{
+    internal class Program
+    {
+        private static int rows = 5;
+        private static int columns = 5;
+
+        private static int[,] islandsArray = new int[rows, columns];
+        private static int[,] visitedArray = new int[rows, columns];
+
+        private static void Main(string[] args)
+        {
+            PopulateArray();
+            PopulateVisited();
+
+            WriteArray(islandsArray);
+            Console.WriteLine("");
+
+            int islandCount = CountTheIslands();
+
+            Console.WriteLine("Number of islands: " + islandCount.ToString());
+            Console.ReadKey();
+        }
+
+        private static int CountTheIslands()
+        {
+            int islandCount = 0;
+
+            for (int row = 0; row < islandsArray.GetLength(0); row++)
+            {
+                for (int col = 0; col < islandsArray.GetLength(1); col++)
+                {
+                    bool isNewIsland = CheckIfItsANewIsland(row, col);
+
+                    if (isNewIsland)
+                    {
+                        islandCount++;
+                    }
+                }
+            }
+
+            return islandCount;
+        }
+
+        private static bool CheckIfItsANewIsland(int row, int col)
+        {
+            if (islandsArray[row, col] == 1)
+            {
+                visitedArray[row, col] = 1;
+
+                //Check for upper neighbor island.
+                if (row != 0)
+                {
+                    if (visitedArray[row - 1, col] == 1)
+                    {
+                        return false;
+                    }
+                }
+
+                //Check for left neighbor island.
+                if (col != 0)
+                {
+                    if (visitedArray[row, col - 1] == 1)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void PopulateArray()
+        {
+            Random r = new Random();
+
+            for (int row = 0; row < islandsArray.GetLength(0); row++)
+            {
+                for (int col = 0; col < islandsArray.GetLength(1); col++)
+                {
+                    islandsArray[row, col] = r.Next(0, 2);
+                }
+            }
+        }
+
+        private static void PopulateVisited()
+        {
+            for (int row = 0; row < islandsArray.GetLength(0); row++)
+            {
+                for (int col = 0; col < islandsArray.GetLength(1); col++)
+                {
+                    visitedArray[row, col] = 0;
+                }
+            }
+        }
+
+        private static void WriteArray(int[,] array)
+        {
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    Console.Write(array[row, col] + " ");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
